c1c10b78fcc3dbc8afb239983c5cacf643b6cf53
enochwick <eshiutopia@gmail.com>
2026-06-04 11:36:19 -0500

    comit

--- page-platform.php
+++ page-platform.php
@@ -21,7 +21,7 @@
 	<div class="phg-scroll" id="phg-scroll">
 		<div class="phg-sticky" id="phg-sticky">
 			<div class="phg-single" id="phg-gallery">
-				<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/dashboard.png' ); ?>" alt="Stride dashboard" loading="eager">
+				<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/platform screenshots/business_central_dashboard_anonymized_clean_v2.png' ); ?>" alt="Stride dashboard" loading="eager">
 			</div>
 		</div>
 	</div>
